57316c707001af62ad444aaf14508a0254f80e76
barrtek02 <barrtek02@gmail.com>
2025-06-21 13:41:10 +0200

    feat: add ae

--- pretrain/ae_pretrain.py
+++ pretrain/ae_pretrain.py
@@ -7,74 +7,60 @@ from typing import Dict, Any
 from src.data.loaders import get_loaders
 from src.utils.params_yaml import load_yaml
 from src.models.resnet18_with_pools import build_resnet18
-from src.models.vae import VAELoss
+from src.models.ae import AELoss  # your AE loss from src/models/ae.py
 
-PROJECT_ROOT: Path = Path(__file__).resolve().parent.parent
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
 params: Dict[str, Any] = load_yaml()
 
-def pretrain_vae():
-
-    paths = params["paths"]
-
-    save_path = PROJECT_ROOT / paths["checkpoints_dir"] / "vae_pretrained.pth"
+def pretrain_ae():
+    save_path = PROJECT_ROOT / params["paths"]["checkpoints_dir"] / "ae_pretrained.pth"
     save_path.parent.mkdir(parents=True, exist_ok=True)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
     train_loader, _ = get_loaders()
 
     model = build_resnet18(
         pretrained=params['model']['pretrained'],
-        pool_type='vae',
+        pool_type='ae',
         freeze_pool=False,
         latent=params['model']['latent_channels'],
         num_classes=params['model']['num_classes'],
     ).to(device)
 
-    vae = model.maxpool
+    ae = model.maxpool
     for name, p in model.named_parameters():
         if not name.startswith('maxpool'):
             p.requires_grad = False
 
     optimizer = optim.AdamW(
-        vae.parameters(),
-        lr=params['vae']['train']['weight_decay'],
+        ae.parameters(),
+        lr=params['ae']['train']['learning_rate'],
         weight_decay=0
     )
-    criterion = VAELoss()
+    criterion = AELoss(recon_loss_weight=params['ae']['recon_loss_weight'])
 
-    epochs = params['vae']['pretrain']['epochs']
-
+    epochs = params['ae']['train']['epochs']
     for epoch in range(1, epochs + 1):
-
-        vae.train()
+        ae.train()
         running_loss = 0.0
-        pbar = tqdm(train_loader, desc=f'[VAE Pretrain] Epoch {epoch}/{epochs}')
+        pbar = tqdm(train_loader, desc=f'[AE Pretrain] Epoch {epoch}/{epochs}')
         for images, _ in pbar:
             images = images.to(device)
 
             with torch.no_grad():
-                x = model.conv1(images)
-                x = model.bn1(x)
-                x = model.relu(x)
-            feats = x
+                feats = model.relu(model.bn1(model.conv1(images)))
 
             optimizer.zero_grad()
-            recon = vae(feats)
-            loss = criterion(vae, recon, feats)
-
+            recon, _ = ae(feats)
+            loss = criterion(recon, feats)
             loss.backward()
             optimizer.step()
 
             running_loss += loss.item()
-            pbar.set_postfix({'vae_loss': running_loss / (pbar.n + 1)})
+            pbar.set_postfix({'ae_loss': running_loss / (pbar.n + 1)})
 
-    save_dir = Path(save_path).parent
-    save_dir.mkdir(parents=True, exist_ok=True)
-    torch.save(vae.state_dict(), save_path)
-
-    print(f'Pretrained VAE saved to {save_path}')
-
+    torch.save(ae.state_dict(), save_path)
+    print(f'Pretrained AE saved to {save_path}')
 
 if __name__ == '__main__':
-    pretrain_vae()
+    pretrain_ae()
